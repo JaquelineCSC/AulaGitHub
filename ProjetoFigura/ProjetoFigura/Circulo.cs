dda44f8cd398f60f65f22e8eb93c3936f3320de0
Lucas <lucasrobert994@gmail.com>
2019-06-04 10:14:22 -0300

    Classe circulo criada

---
+++ ProjetoFigura/ProjetoFigura/Circulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFigura
+{
+    class Circulo:Figura
+    {
+        private double raio;
+
+        public double Raio { get => raio; set => raio = value; }
+
+        public override double Area()
+        {
+           return 3.14 * Math.Pow(raio, 2);
+        }
+
+    }   
+}
